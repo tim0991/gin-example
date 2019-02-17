b82ce03c3f99394b293c8dbacc7be1a65bdb0c01
tim0991 <tim0991@qq.com>
2019-02-17 21:46:58 +0800

    add bind

--- bind/main.go
+++ bind/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,21 @@ type Login struct {
 
 func main() {
 	r := gin.Default()
-	
+
+	r.POST("/loginForm", func(c *gin.Context) {
+		var form Login
+		// This will infer what binder to use depending on the content-type header.
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		
+		if form.User != "manu" || form.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		} 
+		
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
 	r.Run(":3000")
 }
